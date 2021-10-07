725996fde5187f7d36360cbffbe60e85bfca5aa3
Angel Luis <angeluisq96@gmail.com>
2021-10-06 23:23:09 -0300

    Creamos transiciones en desktop

--- index.js
+++ index.js
@@ -11,7 +11,29 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/views/index.html'))
   })
 
+  app.post('/productSearch', (req, res) => {
+    res.redirect('/')
+  })
+
+  app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/views/register.html'))
+  })
+
+  app.post('/registerDone', (req, res) => {
+    res.redirect('/')
+  })
+
+  app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/views/login.html'))
+  })
+
+  app.post('/loginDone', (req, res) => {
+    res.redirect('/')
+  })
+
 app.use(express.static('public'));
+
+app.use(express.urlencoded());
 
 app.listen(port, () => {
 console.log(`Mi aplicacion la levanto por: ${port}`)
